b5e0c4964f5fa88aeca14514afa3a01fa74bcaac
Daniel Gomez <dgomez.informatica@gmail.com>
2022-11-22 14:58:29 -0300

    Ejercicio terminado in console - visual studio 2022

---
+++ numeros_divisores/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace numeros_divisores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero = 0;
+            double divisor = 0;
+            String aux = ""; // Acumula los numeros es un String, para luego mostrarlos
+
+            Console.WriteLine("Ingrese un numero entre el 1 - 100");
+            try
+            {
+                numero = double.Parse(Console.ReadLine());
+                for (int i = 1; i <= numero; i++)
+                {
+                    divisor = numero / i;
+                    if (divisor == Math.Floor(divisor))
+                    {
+                        aux += " " + i;
+                    }
+                }
+                Console.WriteLine("Los divisores son:{0}", aux);
+
+            } 
+            catch (Exception e)
+            {
+                Console.Beep(); 
+                Console.WriteLine($"Error: {e.Message}");
+            }
+
+
+            Console.WriteLine("\nPresione cualquier tecla para salir... ");
+            Console.ReadKey();
+        }
+    }
+}
